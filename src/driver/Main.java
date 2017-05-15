b6892d999b307931bb788675b732999b327ba4a7
brandon-a <babrandon21@gmail.com>
2017-05-14 23:17:53 -0700

    Assignment 3 Requirements

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -1,12 +1,19 @@
 package driver;
+import java.util.LinkedHashMap;
+
 import adapter.*;
+import model.Automobile;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//Build Automobile Object from a file.
 		BuildAuto FordZTW = new BuildAuto();
-		FordZTW.buildAuto("Ford's Focdgfsus Wagon ZTW.txt");
+		FordZTW.buildAuto("Ford's Focus Wagon ZTW.txt");
+		LinkedHashMap<String, Automobile> vehicles = new LinkedHashMap<String, Automobile>(3);
+		vehicles.put("Ford's Focus Wagon ZTW.txt", FordZTW.getAuto());
+		vehicles.put("Ford's Focus Wagon ZTW.txt", FordZTW.getAuto());
+		
 		
 		//Print attributes with invalid file name, check error log for details.
 
